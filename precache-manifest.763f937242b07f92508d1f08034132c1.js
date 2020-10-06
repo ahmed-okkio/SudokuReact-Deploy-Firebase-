36722a89e08234a5f2f13e31e5a3984b6e1113fd
Okkio <60454980+Okkio@users.noreply.github.com>
2020-10-06 13:14:08 +0100

    Updates

--- precache-manifest.763f937242b07f92508d1f08034132c1.js
+++ precache-manifest.763f937242b07f92508d1f08034132c1.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b8ee498c008ff42a1467686c66c751b7",
+    "revision": "afddd6abe3b70bb73867ea4606cf0ba2",
     "url": "/SudokuReact-Deploy/index.html"
   },
   {
-    "revision": "1af895344c200c1b1e83",
-    "url": "/SudokuReact-Deploy/static/css/main.e395e0b6.chunk.css"
+    "revision": "01a7befebec1994553f9",
+    "url": "/SudokuReact-Deploy/static/css/main.d4e1551e.chunk.css"
   },
   {
-    "revision": "9b904cd7c7e7fb968b15",
-    "url": "/SudokuReact-Deploy/static/js/2.8d93da6b.chunk.js"
+    "revision": "5e7e8ff12977b1342d85",
+    "url": "/SudokuReact-Deploy/static/js/2.7188256a.chunk.js"
   },
   {
     "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
-    "url": "/SudokuReact-Deploy/static/js/2.8d93da6b.chunk.js.LICENSE.txt"
+    "url": "/SudokuReact-Deploy/static/js/2.7188256a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1af895344c200c1b1e83",
-    "url": "/SudokuReact-Deploy/static/js/main.c59c5d5e.chunk.js"
+    "revision": "01a7befebec1994553f9",
+    "url": "/SudokuReact-Deploy/static/js/main.6e6ef12d.chunk.js"
   },
   {
     "revision": "f6a197642ae5c68c4450",
